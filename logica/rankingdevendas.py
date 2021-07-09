8698124a2b57fd92204ae89a4002f8daf46896cb
Pedro <pedro.moisesandrade@gmail.com>
2021-07-09 16:28:14 -0300

    Exercício de Ranking

--- logica/rankingdevendas.py
+++ logica/rankingdevendas.py
@@ -4,7 +4,8 @@ Neste cenário, você é desenvolvedor de uma empresa que presta um serviço de
 consultoria jurídica num modelo digital de assinaturas.
 Durante o mês, os vendedores competem em um ranking de vendas.
 O ranking obedece a um único e simples critério, a quantidade de assinaturas vendidas por cada vendedor.
-Não existe critério de desempate, de forma que os vendedores com o mesmo número de vendas ocupam a mesma posição no ranking.
+Não existe critério de desempate, de forma que os vendedores
+com o mesmo número de vendas ocupam a mesma posição no ranking.
 Por exemplo: se 5 vendedores efetuam respectivamente 30, 12, 30, 18 e 12 vendas,
 teríamos um ranking com 2 vendedores ocupando a primeira posição (com 30 vendas),
 1 vendedor na segunda posição (com 18 vendas) e outros 2 vendedores na terceira posição (com 12 vendas).
@@ -29,7 +30,8 @@ A terceira linha contém o número de elementos do vetor C
 A quarta linha contém os elementos do vetor C separados por espaço.
 
 Formato de saída:
-Para cada valor em C, imprima o valor da posição Carlos na simulação, cada valor deve ser impresso em uma nova linha.
+Para cada valor em C, imprima o valor da posição Carlos na simulação,
+cada valor deve ser impresso em uma nova linha.
 
 Exemplo de entrada:
 5
@@ -42,24 +44,71 @@ Exemplo de Saída:
 4
 1
 
-
 test1
 Saída:
 7
-
 55 100 100 40 100 50 35
-
 20 60 40 10
-
 
 test2
 Saída:
 10
-
 30 0 15 20 30 10 10 15 20 0
-
 2
-
 1 40
+
+'''
+#entrada
+# test 1
+'''Saída:
+7
+55 100 100 40 100 50 35
+20 60 40 10'''
+
+# test 2
+'''Saída:
+10
+30 0 15 20 30 10 10 15 20 0
+2
+1 40'''
+V = [30, 0, 15, 20, 30, 10, 10, 15, 20, 0]
+V_set = list(set(V))
+C = [1,40]
+#saida2
+print(len(V))
+print(*V)
+print(len(C))
+print(*C)
+'''#exercício2
+x = 0
+for x in range(len(C)):
+    def codigo(x):
+        V_set.append(C[x])
+        V_set.sort(reverse=True)
+        return V_set.index(C[x]) + 1
+print(codigo(0))
+V_set.pop(V_set.index(C[0]))
+print(codigo(1))
+'''
+
+
+'''while x < len(C):
+    x -= 1
+    def code(x):
+        V_set.append(C[x])
+        V_set.sort(reverse=True)
+        return V_set.index(C[x]) + 1
+    print(code(0))'''
+
+'''
+def code(x):
+    V_set.append(C[x])
+    V_set.sort(reverse=True)
+    return V_set.index(C[x]) + 1
+
+print(code(0))
+V_set.pop(V_set.index(C[0]))
+print(code(1))
+print(code(2))
 
 '''

db776239dc2d32cd186ee861e7ce920f9720baf6
Pedro <pedro.moisesandrade@gmail.com>
2021-07-14 16:41:11 -0300

    Comandas do restaurante

--- logica/comandas_restaurante.py
+++ logica/comandas_restaurante.py
@@ -1,12 +1,6 @@
 '''Comandas em um Restaurante
 
-Comandas em um Restaurante
-
-Contextualização:
-
 Você é responsável pelo sistema de atendimentos e geração de comandas de um restaurante. Todas as mesas geram uma lista de itens consumidos.
-
-
 
 Uma das principais funcionalidades desse sistema é dividir o valor total de consumo pelo número de clientes da mesa, mas uma melhoria foi apontada: nem todos os itens consumidos devem ser incluídos na divisão comum da conta (entre todos os clientes na mesa).
 
@@ -14,21 +8,17 @@ Exemplo:
 
 Em uma mesa com o número de clientes C=4, foram consumidos os seguintes itens com seus respectivos valores:
 
-
-
 bebidas: 150
 entrada: 100
 principal: 400
 sobremesa: 300
 reserva: 40
 
-
 Foi identificado que os itens "bebidas" e "reservas" não vão fazer parte da divisão comum da conta. Podemos concluir então que:
 
 O valor total da conta é 990
 O valor que cada cliente da mesa deve pagar na divisão comum é 200, visto que o total de itens, com exceção dos itens "bebidas" e "reserva", é 800 e devemos dividir esse valor pelos 4 clientes da mesa.
 Por fim, o valor resultante dos itens "bebidas" e "reserva", desconsiderados na divisão comum é 190.
-
 
 Seu desafio:
 
@@ -38,10 +28,7 @@ O valor total da conta;
 O valor que cada cliente deve pagar resultante da divisão comum.
 Por fim, o valor total resultante dos itens que não fizeram parte da divisão comum.
 
-
 Obs.: Todos os valores de saída devem ser convertidos para o tipo "inteiro", evitando problemas de formatação da sua resposta.
-
-
 
 Formato de Entrada (todas as linhas de entrada são do tipo string):
 
@@ -50,29 +37,26 @@ A segunda linha contém a quantidade N de itens consumidos
 As N linhas seguintes trazem cada uma, um item e seu respectivo valor separados por espaço.
 A linha seguinte contém os itens que devem ser desconsiderados na divisão comum, separados por espaço
 
-
 Formato de Saída (todas as linhas de saída devem ser convertidas para o tipo "inteiro"):
 
 A primeira linha deve conter o valor total da conta
 A segunda linha deve conter o valor que cada cliente precisa pagar como resultado da divisão comum.
 A terceira linha deve conter o valor total resultante dos itens que não fizeram parte da divisão comum
 
+                    Exemplo de entrada:
+                        6
+                        4
+                        entrada 150
+                        bebidas 60
+                        principal 240
+                        sobremesa 30
+                        entrada bebidas sobremesa
 
-Exemplo de entrada:
-6
-4
-entrada 150
-bebidas 60
-principal 240
-sobremesa 30
-entrada bebidas sobremesa
+                    Exemplo de saída:
 
-
-Exemplo de saída:'''
-'''
-                        TEST CASE 1
-                        Resultado:
-                        ENTRADAS
+                    TEST CASE 1
+                    Resultado:
+                    ENTRADAS
                         3
                         7
                         item01 200
@@ -84,7 +68,7 @@ Exemplo de saída:'''
                         item07 35
                         item03 item04 item07
 
-                        Saída esperada
+                    Saída esperada:
                         595
                         140
                         175
@@ -97,21 +81,21 @@ Resultado:
 
 
                     TEST 4 
-            ENTRADAS:
-            6
-            4
-            entrada 150
-            bebidas 60
-            principal 240
-            sobremesa 30
-            entrada bebidas sobremesa
+                    ENTRADAS:
+                        6
+                        4
+                        entrada 150
+                        bebidas 60
+                        principal 240
+                        sobremesa 30
+                        entrada bebidas sobremesa
 
-            RESULTADO
-            Saída esperada
-            480
-            40
-            240
-            '''
+                        RESULTADO
+                    Saída esperada
+                        480
+                        40
+                        240
+                        '''
 
 
 #tentativa TEST 4

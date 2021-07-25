e9b4b0a3fa41be1419430c0f2d99d8d99e56fbb6
Pedro <pedro.moisesandrade@gmail.com>
2021-07-25 15:25:37 -0300

    Exercício de remoção de elementos repetidos

---
+++ entrevistas_tecnicas/conjuntos/dedup.py
@@ -0,0 +1,18 @@
+def dedup(lst):
+    """Remove duplicates from lst
+
+    :param lst:
+    :return: new list without duplicated elements
+
+    linear for time and space
+    """
+    result = []
+    repeated=set()
+
+    for ele in lst:
+        if ele not in repeated:
+            result.append(ele)
+            repeated.add(ele)
+    return result
+
+print(dedup(['banana', 'banana', 'banana', 'abacaxi', 'caqui']))
